d2dcbc9bef20e232507917544758a26d08d56f37
Ahmad Alhariri <ahmadmhd357@gmail.com>
2024-03-04 14:18:46 +0300

    first push

---
+++ android/app/src/main/kotlin/com/example/chetchat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chetchat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,146 @@
+import 'dart:ui';
+
+import 'package:chetchat/utils/animations.dart';
+import 'package:flutter/material.dart';
+
+import '../data/bg_data.dart';
+import '../utils/text_utils.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  int selectedIndex = 0;
+  bool showOption = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10),
+        height: 49,
+        width: double.infinity,
+        child: Row(
+          children: [
+            Expanded(
+                child: showOption
+                    ? ShowUpAnimation(
+                        delay: 100,
+                        child: ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: bgList.length,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              return GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    selectedIndex = index;
+                                  });
+                                },
+                                child: CircleAvatar(
+                                  radius: 30,
+                                  backgroundColor: selectedIndex == index
+                                      ? Colors.white
+                                      : Colors.transparent,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(1),
+                                    child: CircleAvatar(
+                                      radius: 30,
+                                      backgroundImage: AssetImage(
+                                        bgList[index],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              );
+                            }),
+                      )
+                    : const SizedBox()),
+            const SizedBox(
+              width: 20,
+            ),
+            showOption
+                ? GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        showOption = false;
+                      });
+                    },
+                    child: const Icon(
+                      Icons.close,
+                      color: Colors.white,
+                      size: 30,
+                    ))
+                : GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        showOption = true;
+                      });
+                    },
+                    child: CircleAvatar(
+                      backgroundColor: Colors.white,
+                      child: Padding(
+                        padding: const EdgeInsets.all(1),
+                        child: CircleAvatar(
+                          radius: 30,
+                          backgroundImage: AssetImage(
+                            bgList[selectedIndex],
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+          ],
+        ),
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(bgList[selectedIndex]), fit: BoxFit.fill),
+        ),
+        alignment: Alignment.center,
+        child: Container(
+          height: 400,
+          width: double.infinity,
+          margin: const EdgeInsets.symmetric(horizontal: 30),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.white),
+            borderRadius: BorderRadius.circular(15),
+            color: Colors.black.withOpacity(0.1),
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaY: 5, sigmaX: 5),
+                child: Padding(
+                  padding: const EdgeInsets.all(25),
+                  child: Form(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: 'Email Address',
+                            labelStyle: TextStyle(color: Colors.white),
+                            suffixIcon: Icon(
+                              Icons.email_rounded,
+                              color: Colors.white,
+                            ),
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                      ],
+                    ),
+                  ),
+                )),
+          ),
+        ),
+      ),
+    );
+  }
+}

adf057ac42944ce814f37bc6d749c1228cd7bd07
Ahmad Alhariri <ahmadmhd357@gmail.com>
2024-05-14 02:03:43 +0300

    splash screen and home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:chetchat/screens/home_screen.dart';
 import 'package:chetchat/screens/login_screen.dart';
+import 'package:chetchat/screens/splash_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -23,7 +26,18 @@ class App extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(
             seedColor: const Color.fromARGB(255, 63, 17, 177)),
       ),
-      home: const LoginScreen(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
+          if (snapshot.hasData) {
+            return const HomeScreen();
+          }
+          return const LoginScreen();
+        },
+      ),
     );
   }
 }
